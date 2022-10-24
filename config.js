9f86665a9fce5198d52cb8ff47953d8975a7176a
Silly-God <104313535+Prithvish-kuamar@users.noreply.github.com>
2022-10-24 23:55:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    app: {
+        playing: 'by Silly-God ❤️',
+        global: true,
+        guild: 'XXX'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        leaveOnEnd: true,
+        loopMessage: false,
+        spotifyBridge: true,
+        defaultvolume: 75,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
